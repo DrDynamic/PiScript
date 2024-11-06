561e468016690e74b2b052f72491ad0073687cdc
Max Weber <mweber@kzvnr.de>
2024-11-06 18:54:33 +0000

    add chunks for bytecode storage

--- src/main.c
+++ src/main.c
@@ -1,38 +1,6 @@
-/**
- * @file main.c
- * @author NikLeberg (niklaus.leuenb@gmail.com)
- * @brief
- * @version 0.1
- * @date 2021-06-22
- *
- * @copyright Copyright (c) 2021 Niklaus Leuenberger
- *
- */
+#include "common.h"
 
-
-/*
- * Includes
- *
- */
-
-#include <stdio.h>
-
-#include "mylib/mylib.h"
-
-
-/*
- * Main program
- *
- */
-
-/**
- * @brief Main
- *
- * @return int allways 0
- */
-int main() {
-    char str[10];
-    mylib_sayHello(str);
-    printf("\n%s", str);
+int main(int argc, const char* argv[])
+{
     return 0;
 }
