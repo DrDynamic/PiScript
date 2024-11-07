36b8984baf57568792a01781a4c1ca1fb61755d3
Max Weber <mweber@kzvnr.de>
2024-11-07 14:18:57 +0000

    add unit tests for memory

--- test/util/memory.c
+++ test/util/memory.c
@@ -53,6 +53,27 @@ static void reallocate_allocates_memory(void **state) {
     free(data);
 }
 
+/**
+ * @brief Function reallocate should reallocate memory, when it is already
+ * allocated.
+ *
+ * @param state unused
+ */
+static void reallocate_reallocates_memory(void **state) {
+    (void)state;
+
+    void *data = NULL;
+
+    expect_value(__wrap_realloc, size, 16);
+    data = reallocate(data, 8, 16);
+    free(data);
+}
+
+/**
+ * @brief Function reallocate should free memory, when it new size is 0.
+ *
+ * @param state unused
+ */
 static void reallocate_frees_memory(void **state) {
     (void)state;
 
@@ -60,6 +81,51 @@ static void reallocate_frees_memory(void **state) {
     data = reallocate(data, 8, 0);
 }
 
+/**
+ * @brief Macro GROW_CAPACITY returns a minimum value of 8, when the given capacity is smaler.
+ *
+ * @param state unused
+ */
+static void GROW_CAPACITY_has_minimum_capacity(void **state) {
+    (void)state;
+
+    assert_int_equal(GROW_CAPACITY(0), 8);
+    assert_int_equal(GROW_CAPACITY(7), 8);
+}
+
+/**
+ * @brief Macro GROW_CAPACITY returns the doulbe of the given capacity, when the given capacity is
+ * bigger or equal to 8.
+ *
+ * @param state unused
+ */
+static void GROW_CAPACITY_doubles_capacity(void **state) {
+    (void)state;
+
+    assert_int_equal(GROW_CAPACITY(8), 16);
+    assert_int_equal(GROW_CAPACITY(16), 32);
+}
+
+/**
+ * @brief Macro GROW_CAPACITY returns the doulbe of the given capacity, when the given capacity is
+ * bigger or equal to 8.
+ *
+ * @param state unused
+ */
+static void GROW_ARRAY_reallocates_memory(void **state) {
+    (void)state;
+
+    uint8_t *data = NULL;
+
+    expect_value(__wrap_realloc, size, 8);
+    data = GROW_ARRAY(uint8_t, data, 0, 8);
+
+    expect_value(__wrap_realloc, size, 16);
+    data = GROW_ARRAY(uint8_t, data, 8, 16);
+
+    // no realloc -> calls free()
+    data = GROW_ARRAY(uint8_t, data, 16, 0);
+}
 
 /*
  * Main test program
@@ -74,6 +140,11 @@ static void reallocate_frees_memory(void **state) {
 int main(void) {
     const struct CMUnitTest tests_memory[] = {
         cmocka_unit_test(reallocate_allocates_memory),
-        cmocka_unit_test(reallocate_frees_memory)};
+        cmocka_unit_test(reallocate_reallocates_memory),
+        cmocka_unit_test(reallocate_frees_memory),
+        cmocka_unit_test(GROW_CAPACITY_has_minimum_capacity),
+        cmocka_unit_test(GROW_CAPACITY_doubles_capacity),
+        cmocka_unit_test(GROW_ARRAY_reallocates_memory),
+    };
     return cmocka_run_group_tests(tests_memory, NULL, NULL);
 }
