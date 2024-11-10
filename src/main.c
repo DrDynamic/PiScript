beadeb0311432c151a36e3186d803e47f0ee3a10
Max Weber <mweber@kzvnr.de>
2024-11-10 17:37:50 +0000

    add arithmetic binary operators

--- src/main.c
+++ src/main.c
@@ -19,9 +19,17 @@ int main(int argc, const char* argv[])
     for (int i = 0; i < 0x1FF; i++) {
         //        writeConstant(&chunk, i, 1);
     }
-    writeConstant(&chunk, 13.37, 1);
+    writeConstant(&chunk, 13, 1);
     writeChunk(&chunk, OP_NEGATE, 1);
-    writeChunk(&chunk, OP_RETURN, 2);
+    writeConstant(&chunk, 26, 2);
+    writeChunk(&chunk, OP_ADD, 2);
+    writeConstant(&chunk, 11, 2);
+    writeChunk(&chunk, OP_SUBTRACT, 2);
+    writeConstant(&chunk, 5, 2);
+    writeChunk(&chunk, OP_MULTIPLY, 2);
+    writeConstant(&chunk, 4, 2);
+    writeChunk(&chunk, OP_DIVIDE, 2);
+    writeChunk(&chunk, OP_RETURN, 3);
 
     //    disassambleChunk(&chunk, "test chunk");
 
--- src/vm.c
+++ src/vm.c
@@ -24,6 +24,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                                              \
+    do {                                                                                           \
+        double b = pop();                                                                          \
+        double a = pop();                                                                          \
+        push(a op b);                                                                              \
+    } while (false)
+
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
@@ -38,6 +45,18 @@ static InterpretResult run()
 
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
         case OP_NEGATE:
             push(-pop());
             break;
@@ -68,6 +87,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk)
@@ -80,12 +100,14 @@ InterpretResult interpret(Chunk* chunk)
 
 void push(Value value)
 {
+    // TODO: check for stackoverflow
     *vm.stackTop = value;
     vm.stackTop++;
 }
 
 Value pop()
 {
+    // TODO: chack for stack underflow?
     vm.stackTop--;
     return *vm.stackTop;
 }
