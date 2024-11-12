ab811c33cfafdc0f822dea5395cc4a5bfcd7f36e
Max Weber <mweber@kzvnr.de>
2024-11-12 08:59:11 +0000

    scaffold scanning

--- src/main.c
+++ src/main.c
@@ -1,10 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
-#include "chunk/chunk.h"
-#include "util/debug.h"
 #include "vm.h"
 
-// playground includes
-#include <stdio.h>
+
+static void repl()
+{
+    char line[1024];
+
+    for (;;) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path)
+{
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path)
+{
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR)
+        exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR)
+        exit(70);
+}
 
 int main(int argc, const char* argv[])
 {
@@ -13,30 +66,15 @@ int main(int argc, const char* argv[])
 
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
-
-    for (int i = 0; i < 0x1FF; i++) {
-        //        writeConstant(&chunk, i, 1);
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: pit [pasth]\n");
+        exit(64);
     }
-    writeConstant(&chunk, 13, 1);
-    writeChunk(&chunk, OP_NEGATE, 1);
-    writeConstant(&chunk, 26, 2);
-    writeChunk(&chunk, OP_ADD, 2);
-    writeConstant(&chunk, 11, 2);
-    writeChunk(&chunk, OP_SUBTRACT, 2);
-    writeConstant(&chunk, 5, 2);
-    writeChunk(&chunk, OP_MULTIPLY, 2);
-    writeConstant(&chunk, 4, 2);
-    writeChunk(&chunk, OP_DIVIDE, 2);
-    writeChunk(&chunk, OP_RETURN, 3);
-
-    //    disassambleChunk(&chunk, "test chunk");
-
-    interpret(&chunk);
 
     freeVM();
-    freeChunk(&chunk);
-
     return 0;
 }
