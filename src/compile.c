b02da66212e2a6e081a5a3ce647b7e1b417baa9c
Max Weber <mweber@kzvnr.de>
2025-03-20 17:01:01 +0000

    define variables

--- src/compile.c
+++ src/compile.c
@@ -129,6 +129,12 @@ static void emitReturn()
     emitByte(OP_RETURN);
 }
 
+static uint32_t makeConstant(Value value)
+{
+    uint32_t constant = addConstant(currentChunk(), value);
+    return constant;
+}
+
 static void emitConstant(Value value, int line)
 {
     writeConstant(currentChunk(), value, line);
@@ -307,6 +313,25 @@ static void parsePrecedence(Precedence precedence)
     }
 }
 
+static uint32_t identifierConstant(Token* name)
+{
+    return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage)
+{
+    consume(TOKEN_IDENTIFIER, errorMessage);
+    return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint32_t global)
+{
+
+    // TODO: support OP_CONSTANT_LONG
+
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static ParseRule* getRule(TokenType type)
 {
     return &rules[type];
@@ -315,6 +340,21 @@ static ParseRule* getRule(TokenType type)
 static void expression()
 {
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void varDeclaration()
+{
+    uint8_t global = parseVariable("Expect variable name.");
+
+    if (match(TOKEN_EQUAL)) {
+        expression();
+    } else {
+        emitByte(OP_NIL);
+    }
+    match(TOKEN_SEMICOLON);
+    //    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+
+    defineVariable(global);
 }
 
 static void expressionStatement()
@@ -359,7 +399,11 @@ static void synchronize()
 
 static void declaration()
 {
-    statement();
+    if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        statement();
+    }
 
     if (parser.panicMode)
         synchronize();
