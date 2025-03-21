3185657b939c95ee619a0cb42d9ea8fee6c2add2
Max Weber <mweber@kzvnr.de>
2025-03-21 14:19:29 +0000

    fix edgecase assigning avariable in own initializer

--- src/compiler.c
+++ src/compiler.c
@@ -421,6 +421,9 @@ static int resolveLocal(Compiler* compiler, Token* name)
     for (int i = compiler->localCount - 1; i >= 0; i--) {
         Local* local = &compiler->locals[i];
         if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("Can't read local variable in its own initializer.");
+            }
             return i;
         }
     }
@@ -436,7 +439,7 @@ static void addLocal(Token name)
     }
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable()
@@ -472,9 +475,15 @@ static uint32_t parseVariable(const char* errorMessage)
     return identifierConstant(&parser.previous);
 }
 
+static void markInitialized()
+{
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint32_t global)
 {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
     emitConstant(global, parser.previous.line, OP_DEFINE_GLOBAL, OP_DEFINE_GLOBAL_LONG);
