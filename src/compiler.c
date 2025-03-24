a8ed3084d069e2d406a61532710a4e9fbff47972
Max Schon <mschon@kzvnr.de>
2025-03-24 19:09:18 +0000

    add logicat and / or

--- src/compiler.c
+++ src/compiler.c
@@ -335,6 +335,19 @@ static void number(bool canAssign)
     emitConstant(addr, parser.previous.line, OP_CONSTANT, OP_CONSTANT_LONG);
 }
 
+static void or_(bool canAssign)
+{
+    (void)canAssign;
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+
+    patchJump(elseJump);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_OR);
+    patchJump(endJump);
+}
+
 static void string(bool canAssign)
 {
     (void)canAssign;
@@ -419,7 +432,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER] = { variable, NULL, PREC_NONE },
     [TOKEN_STRING] = { string, NULL, PREC_NONE },
     [TOKEN_NUMBER] = { number, NULL, PREC_NONE },
-    [TOKEN_AND] = { NULL, NULL, PREC_NONE },
+    [TOKEN_AND] = { NULL, and_, PREC_AND },
     [TOKEN_CLASS] = { NULL, NULL, PREC_NONE },
     [TOKEN_ELSE] = { NULL, NULL, PREC_NONE },
     [TOKEN_FALSE] = { literal, NULL, PREC_NONE },
@@ -427,7 +440,7 @@ ParseRule rules[] = {
     [TOKEN_FUN] = { NULL, NULL, PREC_NONE },
     [TOKEN_IF] = { NULL, NULL, PREC_NONE },
     [TOKEN_NIL] = { literal, NULL, PREC_NONE },
-    [TOKEN_OR] = { NULL, NULL, PREC_NONE },
+    [TOKEN_OR] = { NULL, or_, PREC_OR },
     [TOKEN_PRINT] = { NULL, NULL, PREC_NONE },
     [TOKEN_RETURN] = { NULL, NULL, PREC_NONE },
     [TOKEN_SUPER] = { NULL, NULL, PREC_NONE },
@@ -548,6 +561,17 @@ static void defineVariable(uint32_t global)
         return;
     }
     emitConstant(global, parser.previous.line, OP_DEFINE_GLOBAL, OP_DEFINE_GLOBAL_LONG);
+}
+
+static void and_(bool canAssign)
+{
+    (void)canAssign;
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_AND);
+
+    patchJump(endJump);
 }
 
 static ParseRule* getRule(TokenType type)
